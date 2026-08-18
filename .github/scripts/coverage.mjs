bd7c2a1c1bbfc142ed14ec8a82046798f3bc96c0
Anton Medvedev <anton@medv.io>
2026-08-18 22:22:22 +0200

    Add actions

---
+++ .github/scripts/coverage.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env zx
+
+const expected = 80
+const exclude = [
+  'benchmarks', // Benchmark-only package, not part of the main codebase.
+]
+
+cd(path.resolve(__dirname, '..', '..'))
+
+await spinner('Running tests', async () => {
+  await $`go test -coverprofile=coverage.out -coverpkg=github.com/antonmedv/medb/... ./...`
+  const coverage = fs.readFileSync('coverage.out').toString()
+    .split('\n')
+    .filter(line => {
+      for (const ex of exclude)
+        if (line.includes(ex)) return false
+      return true
+    })
+    .join('\n')
+  fs.writeFileSync('coverage.out', coverage)
+  await $`go tool cover -html=coverage.out -o coverage.html`
+})
+
+const cover = await $({verbose: true})`go tool cover -func=coverage.out`
+const total = +cover.stdout.match(/total:\s+\(statements\)\s+(\d+\.\d+)%/)[1]
+if (total < expected) {
+  echo(chalk.red(`Coverage is too low: ${total}% < ${expected}% (expected)`))
+  process.exit(1)
+} else {
+  echo(`Coverage is good: ${chalk.green(total + '%')} >= ${expected}% (expected)`)
+}

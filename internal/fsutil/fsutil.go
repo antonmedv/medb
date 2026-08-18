42e1959982b7dff4a2481ee366b88eab45e870ee
Anton Medvedev <anton@medv.io>
2026-08-18 22:09:19 +0200

    Add fsutil

---
+++ internal/fsutil/fsutil.go
@@ -0,0 +1,38 @@
+package fsutil
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func WriteFile(path string, data []byte) error {
+	tmp := path + ".tmp"
+	f, err := os.Create(tmp)
+	if err != nil {
+		return err
+	}
+	if _, err := f.Write(data); err != nil {
+		f.Close()
+		return err
+	}
+	if err := f.Sync(); err != nil {
+		f.Close()
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+	if err := os.Rename(tmp, path); err != nil {
+		return err
+	}
+	return SyncDir(filepath.Dir(path))
+}
+
+func SyncDir(path string) error {
+	d, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	return d.Sync()
+}
